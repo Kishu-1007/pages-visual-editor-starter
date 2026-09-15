b21c08c5815286c83455c0f5ad244523e3cd2cff
Kishu-1007 <147312615+Kishu-1007@users.noreply.github.com>
2026-09-15 20:12:20 +0530

    with repo provided vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,46 +3,59 @@ import react from "@vitejs/plugin-react";
 import yextSSG from "@yext/pages/vite-plugin";
 import { yextVisualEditorPlugin } from "@yext/visual-editor/plugin"
 
-export default defineConfig(({ command, mode }) => {
-  // Evaluates true when compiling on the remote cloud sandbox cluster
-  const isSandboxEnv = process.env.NODE_ENV === 'production' || mode === 'production';
 
-
-  return {
-    // CONDITIONAL PLUGINS SETUP:
-    // When deploying to the sandbox platform, completely omit yextVisualEditorPlugin.
-    // This strips out the middleware that triggers the broken 200 websocket handshake.
-
-    plugins: [
-      react(),
-      ...(!isSandboxEnv ? [yextVisualEditorPlugin({ localEditor: { enabled: true } })] : []),
-      yextSSG()
-    ],
-
-    // server: {
-    //   port: 8080,
-    //   // strictPort: true,
-    //   host: 'localhost',
-    //   hmr: isSandboxEnv ? false : {
-    //     protocol: 'ws',
-    //     host: 'localhost',
-    //     port: 8080
-    //   }
-    // },
-
-
-    optimizeDeps: {
-      esbuildOptions: {
-        target: "es2022",
+export default defineConfig({
+  plugins: [
+    react(),
+    yextVisualEditorPlugin({
+      localEditor: {
+        enabled: true
       },
-    },
-    build: {
-      target: "es2022",
-    },
-
-    // Add this block to force the library test router into active state
-    // define: {
-    //   __VISUAL_EDITOR_TEST__: JSON.stringify(false),
-    // }
-  };
+    }),
+    yextSSG(),
+  ],
 });
+
+// export default defineConfig(({ command, mode }) => {
+//   // Evaluates true when compiling on the remote cloud sandbox cluster
+//   const isSandboxEnv = process.env.NODE_ENV === 'production' || mode === 'production';
+
+
+//   return {
+//     // CONDITIONAL PLUGINS SETUP:
+//     // When deploying to the sandbox platform, completely omit yextVisualEditorPlugin.
+//     // This strips out the middleware that triggers the broken 200 websocket handshake.
+
+//     plugins: [
+//       react(),
+//       ...(!isSandboxEnv ? [yextVisualEditorPlugin({ localEditor: { enabled: true } })] : []),
+//       yextSSG()
+//     ],
+
+//     // server: {
+//     //   port: 8080,
+//     //   // strictPort: true,
+//     //   host: 'localhost',
+//     //   hmr: isSandboxEnv ? false : {
+//     //     protocol: 'ws',
+//     //     host: 'localhost',
+//     //     port: 8080
+//     //   }
+//     // },
+
+
+//     optimizeDeps: {
+//       esbuildOptions: {
+//         target: "es2022",
+//       },
+//     },
+//     build: {
+//       target: "es2022",
+//     },
+
+//     // Add this block to force the library test router into active state
+//     // define: {
+//     //   __VISUAL_EDITOR_TEST__: JSON.stringify(false),
+//     // }
+//   };
+// });
